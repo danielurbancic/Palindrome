49113fa9db8ff2a48a076a8d9dcc8a6af66e0bc1
danielurbancic <urbancic.daniel@gmail.com>
2018-03-08 13:44:23 +0100

    ClickON
    OnClick Methode wurde eingefügt. Diese soll dann die Lösung des eingegebenen Strings ausgeben

--- app/src/main/java/com/example/user/palindrome/Palindrome.java
+++ app/src/main/java/com/example/user/palindrome/Palindrome.java
@@ -2,26 +2,41 @@ package com.example.user.palindrome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class Palindrome extends AppCompatActivity {
+
+    EditText palindrome_in;
+    EditText button;
+    EditText ausgabe_ispal;
+    EditText ausgabe_nopal;
+    EditText ausgabe_space;
+    EditText ausgabe_wordlength;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palindrome);
 
-
-
-
-        EditText palindrome_in = (EditText);
-        EditText button = (EditText);
-        EditText ausgabe_ispal = (EditText);
-        EditText ausgabe_nopal = (EditText);
-        EditText ausgabe_space = (EditText);
-        EditText ausgabe_wordlenght = (EditText);
+        palindrome_in = (EditText) findViewById(R.id.palindrome_in);
+        button = (EditText) findViewById(R.id.button);
+        ausgabe_ispal = (EditText) findViewById(R.id.ausgabe_ispal);
+        ausgabe_nopal = (EditText) findViewById(R.id.ausgabe_nopal);
+        ausgabe_space = (EditText) findViewById(R.id.ausgabe_space);
+        ausgabe_wordlength = (EditText) findViewById(R.id.ausgabe_wordlength);
         
 
+    }
+    Button check = (Button) findViewById(R.id.check);
+    check.setOnClickListener(new View.OnClickListener){
+        @Override
+                public void onClick (View view){
+
+        }
 
     }
+
 }
