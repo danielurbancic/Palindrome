16f728039a03516de4b9622dbb174555a8e43c04
danielurbancic <urbancic.daniel@gmail.com>
2018-03-08 15:12:57 +0100

    Ausbesserung
    Ausbesserungen von den deklarationen

--- app/src/main/java/com/example/user/palindrome/Palindrome.java
+++ app/src/main/java/com/example/user/palindrome/Palindrome.java
@@ -5,15 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Palindrome extends AppCompatActivity {
 
     EditText palindrome_in;
-    EditText button;
-    EditText ausgabe_ispal;
-    EditText ausgabe_nopal;
-    EditText ausgabe_space;
-    EditText ausgabe_wordlength;
+    Button button;
+    TextView ausgabe_ispal;
+    TextView ausgabe_nopal;
+    TextView ausgabe_space;
+    TextView ausgabe_wordlength;
 
 
     @Override
@@ -22,11 +23,11 @@ public class Palindrome extends AppCompatActivity {
         setContentView(R.layout.activity_palindrome);
 
         palindrome_in = (EditText) findViewById(R.id.palindrome_in);
-        button = (EditText) findViewById(R.id.button);
-        ausgabe_ispal = (EditText) findViewById(R.id.ausgabe_ispal);
-        ausgabe_nopal = (EditText) findViewById(R.id.ausgabe_nopal);
-        ausgabe_space = (EditText) findViewById(R.id.ausgabe_space);
-        ausgabe_wordlength = (EditText) findViewById(R.id.ausgabe_wordlength);
+        button = (Button) findViewById(R.id.button);
+        ausgabe_ispal = (TextView) findViewById(R.id.ausgabe_ispal);
+        ausgabe_nopal = (TextView) findViewById(R.id.ausgabe_nopal);
+        ausgabe_space = (TextView) findViewById(R.id.ausgabe_space);
+        ausgabe_wordlength = (TextView) findViewById(R.id.ausgabe_wordlength);
 
 
     }
