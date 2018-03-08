cf2d6adc3d709773bd8d6beba7cc61bfc553bd2e
danielurbancic <urbancic.daniel@gmail.com>
2018-03-08 12:27:51 +0100

    Alles
    Das Projekt wurde erstellt und das Design wurde am anfang etwas gemacht. Beim Hauptprogramm wurden die benötigten attribute eingebettet

---
+++ app/src/main/java/com/example/user/palindrome/Palindrome.java
@@ -0,0 +1,27 @@
+package com.example.user.palindrome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class Palindrome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_palindrome);
+
+
+
+
+        EditText palindrome_in = (EditText);
+        EditText button = (EditText);
+        EditText ausgabe_ispal = (EditText);
+        EditText ausgabe_nopal = (EditText);
+        EditText ausgabe_space = (EditText);
+        EditText ausgabe_wordlenght = (EditText);
+        
+
+
+    }
+}
